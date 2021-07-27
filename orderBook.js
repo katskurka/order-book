7d3f34fc21880066f4adc18d99630f400c8ecc8b
katskurka <kati.skurka@gmail.com>
2021-07-27 12:33:52 -0400

    figured out why tests weren't passing and removed the issue, all 9 tests pass now

--- orderBook.js
+++ orderBook.js
@@ -1,16 +1,46 @@
-// reconcileOrder fx that will properly reconcile incoming // //// crypto order given the existing book of orders
 
+
+/* eslint-disable max-len */
 function reconcileOrder(existingBook, incomingOrder) {
-  // first see if the existing book is empty
+  let updatedBook = []
+
   if (existingBook.length <= 0) return [incomingOrder]
-  // Go thru existing book with a for/of loop, also getting iteration indexes 
+
   for (let i = 0; i < existingBook.length; i++) {
     if (incomingOrder.type === existingBook[i].type) {
       existingBook.push(incomingOrder)
 
       return existingBook
     }
+
+    if ((incomingOrder.type !== existingBook[i].type) && (incomingOrder.price !== existingBook[i].price)) {
+      existingBook.push(incomingOrder)
+
+      return existingBook
+    }
+
+    if ((incomingOrder.type !== existingBook.type) && (incomingOrder.quantity === existingBook[i].quantity) && (incomingOrder.price === existingBook[i].price)) {
+      existingBook.splice(i, 1)
+
+      return existingBook
+    }
+    if ((incomingOrder.type !== existingBook[i].type) && (incomingOrder.price === existingBook[i].price) && (incomingOrder.quantity < existingBook[i].quantity)) {
+      existingBook[i].quantity -= incomingOrder.quantity
+      existingBook.push(...existingBook.splice(i, 1))
+
+      return existingBook
+    }
+    if ((incomingOrder.type !== existingBook[i].type) && (incomingOrder.price === existingBook[i].price) && (incomingOrder.quantity > existingBook[i].quantity)) {
+      incomingOrder.quantity -= existingBook[i].quantity
+      existingBook.splice(i, 1)
+
+      return reconcileOrder(existingBook, incomingOrder)
+    }
   }
+
+  return updatedBook
 }
 
 module.exports = reconcileOrder
+
+
